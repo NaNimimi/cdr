5ad1727dd0355fbf3c1c4a35b2be0a85a19c45e6
noname <sherbek123gg@gmail.com>
2026-02-23 20:00:49 +0500

    api

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\OrderController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\OrderItemController;
 
 /*
 |--------------------------------------------------------------------------
@@ -37,3 +38,14 @@ Route::get('/orders/{id}', [OrderController::class, 'show']);
 Route::post('/orders', [OrderController::class, 'store']);
 Route::put('/orders/{id}', [OrderController::class, 'update']);
 Route::delete('/orders/{id}', [OrderController::class, 'destroy']);
+
+/*
+|--------------------------------------------------------------------------
+| OrderItem Routes
+|--------------------------------------------------------------------------
+*/
+Route::get('/order-items', [OrderItemController::class, 'index']);
+Route::get('/order-items/{id}', [OrderItemController::class, 'show']);
+Route::post('/order-items', [OrderItemController::class, 'store']);
+Route::put('/order-items/{id}', [OrderItemController::class, 'update']);
+Route::delete('/order-items/{id}', [OrderItemController::class, 'destroy']);
