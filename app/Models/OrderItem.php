6a3537dc71bb6d66c373696f479612c4c166e810
noname <sherbek123gg@gmail.com>
2026-02-23 19:37:40 +0500

    OrderItemModel

---
+++ app/Models/OrderItem.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Models;
+use Illuminate\Database\Eloquent\Model;
+use Prettus\Repository\Contracts\Transformable;
+use Prettus\Repository\Traits\TransformableTrait;
+
+class OrderItem extends Model implements Transformable {
+
+    use TransformableTrait;
+
+    protected $fillable = ['order_id', 'product_id', 'quantity', 'price'];
+
+
+    
+    public function product() { return $this->belongsTo(Product::class); }
+}
